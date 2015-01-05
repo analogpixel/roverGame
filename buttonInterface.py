9e8a4cb93ac7c2bf8654129eaa0c0886ae25290c
Analog <analog@analogpixel.org>
2015-01-05 14:12:39 -0700

    bounctime in wrong place

--- buttonInterface.py
+++ buttonInterface.py
@@ -9,8 +9,8 @@ def configGPIO(config):
   GPIO.setmode(GPIO.BCM)
 
   for opt in config:
-    GPIO.setup( int(opt['in']) , GPIO.IN, pull_up_down=GPIO.PUD_UP,bouncetime=400)
-    GPIO.add_event_detect( int(opt['in']) , GPIO.FALLING)
+    GPIO.setup( int(opt['in']) , GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.add_event_detect( int(opt['in']) , GPIO.FALLING,bouncetime=400)
 
 def pollGPIO(config, robot):
   for opt in config:
