26199cf4c474ac104c5f457668d8d4041ff45573
Analog <analog@analogpixel.org>
2015-01-05 14:11:00 -0700

    fix activate and add bounce time

--- buttonInterface.py
+++ buttonInterface.py
@@ -9,13 +9,15 @@ def configGPIO(config):
   GPIO.setmode(GPIO.BCM)
 
   for opt in config:
-    GPIO.setup( int(opt['in']) , GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.setup( int(opt['in']) , GPIO.IN, pull_up_down=GPIO.PUD_UP,bouncetime=400)
     GPIO.add_event_detect( int(opt['in']) , GPIO.FALLING)
-
 
 def pollGPIO(config, robot):
   for opt in config:
     if GPIO.event_detected(int(opt['in'])):
-      robot = pushQ(robot, opt['command'])
+      if opt['command'] == "activate":
+        robot = activate(robot)
+      else:
+        robot = pushQ(robot, opt['command'])
   return robot
     
